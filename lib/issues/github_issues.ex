828be749fb9dd70017036f28eff90d715e533a48
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2021-08-05 19:06:05 +0200

    add logging

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,7 +1,11 @@
 defmodule Issues.GitHubIssues do
   @github_url Application.get_env(:issues, :github_url)
 
+  require Logger
+
   def fetch(user, project) do
+
+    Logger.info("Fetching #{user}'s project #{project}")
     url(user, project)
       |> HTTPoison.get([ {"User-agent", "Elixir dave@pragprog.com"} ])
       |> handle_response
@@ -12,6 +16,8 @@ defmodule Issues.GitHubIssues do
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.debug("Got status code: #{status_code}")
+
     {
       status_code |> check_for_error,
       body |> Poison.Parser.parse!
