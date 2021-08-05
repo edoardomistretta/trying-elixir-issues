9fe9164f93f194dc5514f17133012f6dad04d39e
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2021-08-05 21:14:33 +0200

    add docs library

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      name: "Issues",
+      source_url: "https://github.com/edoardomistretta/trying-elixir-issues",
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
@@ -22,10 +24,10 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.0.0"},
-      {:poison, "~> 3.1"},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      { :httpoison, "~> 1.0.0"},
+      { :poison, "~> 3.1"},
+      { :ex_doc, "~> 0.25.1" },
+      { :earmark, "~> 1.4"}
     ]
   end
 
